87b1d256a688f342b1201a90e6370b5708044675
Ana Hobden <operator@hoverbear.org>
2021-11-18 19:39:11 -0800

    Support flake
    Signed-off-by: Ana Hobden <operator@hoverbear.org>

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Hoverbear.org";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    in
+    {
+      checks = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          format = pkgs.runCommand "check-format"
+            { } ''
+            ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt --check ${./.}
+            touch $out # it worked!
+          '';
+        });
+
+      devShell = forAllSystems (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixpkgs-fmt
+            zola
+          ];
+        });
+    };
+}
